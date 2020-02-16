7b5852ff39e7f27b4db5e92f827228de009b40a7
Mark Pashmfouroush <mark@markpash.me>
2020-02-16 18:31:45 +0000

    minor fixes and improvements

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
-curl -fSL -o "./blackbox_exporter-$1.$2.tar.gz" \
-  "https://github.com/prometheus/blackbox_exporter/releases/download/v$1/blackbox_exporter-$1.$2.tar.gz"
+curl -fSL -o "./blackbox_exporter-$1.linux-$2.tar.gz" \
+  "https://github.com/prometheus/blackbox_exporter/releases/download/v$1/blackbox_exporter-$1.linux-$2.tar.gz"
 
-tar xzvf ./blackbox_exporter-$1.$2.tar.gz
+tar xzvf ./blackbox_exporter-$1.linux-$2.tar.gz
 
 # get binaries
 mkdir ./bin
-cp ./blackbox_exporter-$1.$2/blackbox_exporter ./bin/
+cp ./blackbox_exporter-$1.linux-$2/blackbox_exporter ./bin/
 
 # Cp config files
-cp ./blackbox_exporter-$1.$2/blackbox.yml ./
+cp ./blackbox_exporter-$1.linux-$2/blackbox.yml ./
--- package.sh
+++ package.sh
@@ -2,13 +2,8 @@
 
 ./fetch.sh $1 $2
 
-sed -i "s/vx.y.z/v$1/g" ./nfpm.yaml
+sed "s/vx.y.z/v$1/g" ./nfpm.yaml > ./nfpm-test.yaml
 
-nfpm pkg --target blackbox_exporter-$1.$2.deb
+nfpm pkg --target blackbox-exporter_$1_$2.deb
 
-rm -rf ./bin
-rm -rf ./blackbox.yml
-rm -rf ./blackbox_exporter-$1.$2.tar.gz
-rm -rf ./blackbox_exporter-$1.$2
-
-git checkout -- nfpm.yaml
+rm -rf ./bin ./blackbox.yml ./blackbox_exporter-$1.linux-$2 ./blackbox_exporter-$1.linux-$2.tar.gz ./nfpm-test.yaml
