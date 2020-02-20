ecbfd3f9f8f798e67e021f589ace7815a5bd3923
Mark Pashmfouroush <mark@markpash.me>
2020-02-21 00:55:08 +0100

    fix builds

--- package.sh
+++ package.sh
@@ -2,8 +2,8 @@
 
 ./fetch.sh $1 $2
 
-sed "s/vx.y.z/v$1/g" ./nfpm.yaml > ./nfpm-test.yaml
+sed "s/vx.y.z/v$1/g" ./nfpm.yaml > ./nfpm-temp.yaml
 
-nfpm pkg --target blackbox-exporter_$1_$2.deb
+nfpm pkg --config="nfpm-temp.yaml" --target="blackbox-exporter_$1_$2.deb"
 
-rm -rf ./bin ./blackbox.yml ./blackbox_exporter-$1.linux-$2 ./blackbox_exporter-$1.linux-$2.tar.gz ./nfpm-test.yaml
+rm -rf ./bin ./blackbox.yml ./blackbox_exporter-$1.linux-$2 ./blackbox_exporter-$1.linux-$2.tar.gz ./nfpm-temp.yaml
