54ded54319f6b36e2e0014fbe9a67f9c4c986d0f
Francis Begyn <francis@begyn.be>
2022-09-17 23:25:56 +0200

    update to new nfpm layout

--- package.sh
+++ package.sh
@@ -1,9 +1,11 @@
-#!/bin/env bash
+#! /usr/bin/env bash
 
 ./fetch.sh $1 $2
 
 sed "s/vx.y.z/v$1/g" ./nfpm.yaml > ./nfpm-temp.yaml
 
-nfpm pkg --config="nfpm-temp.yaml" --target="blackbox-exporter_$1_$2.deb"
+nfpm pkg --packager deb --config="nfpm-temp.yaml"
+nfpm pkg --packager rpm --config="nfpm-temp.yaml"
 
-rm -rf ./bin ./blackbox.yml ./blackbox_exporter-$1.linux-$2 ./blackbox_exporter-$1.linux-$2.tar.gz ./nfpm-temp.yaml
+rm -rf ./blackbox_exporter ./blackbox.yml ./blackbox_exporter-$1.linux-$2 \
+	./LICENSE ./NOTICE ./blackbox_exporter-$1.linux-$2.tar.gz ./nfpm-temp.yaml
